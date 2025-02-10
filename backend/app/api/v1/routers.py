40fb547d2dce23e06d4facd0e3bf652ab4db0a59
Dmitriy Aleksandrov <Adsis369@gmail.com>
2025-02-10 17:58:14 +0500

    Fix routes after rename

--- backend/app/api/v1/routers.py
+++ backend/app/api/v1/routers.py
@@ -1,6 +1,6 @@
-from app.api.v1.endpoints import dancer
+from app.api.v1.endpoints import users
 from fastapi import APIRouter
 
 api_router = APIRouter()
 
-api_router.include_router(router=dancer.router, prefix="/users", tags=["users"])
+api_router.include_router(router=users.router, prefix="/users", tags=["users"])
